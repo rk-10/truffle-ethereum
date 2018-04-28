982fc18bffbacc5296d6aae0e5dd4ee59ca8e6c6
rk-10 <khanejarohan07@gmail.com>
2018-04-28 18:35:16 +0530

    updated and fixed tests

--- config.js
+++ config.js
@@ -3,10 +3,10 @@
  */
 
 module.exports = {
-    asset : "",
-    buyPrice: "",
-    sellPrice: "",
-    units: "",
+    asset : "0x879166e72813d539f1135b1b67e24928e3d33659",
+    buyPrice: 0,
+    sellPrice: 10000000000000000,
+    units: 5,
     selling: true,
     buying: true,
     ganacheMnemonic : "lounge first learn cheap tuition tape regret swarm sock blanket borrow quarter",
--- test/state tests.js
+++ test/state tests.js
@@ -5,15 +5,68 @@
 var TokenTrader = artifacts.require("TokenTrader");
 
 contract("State storage tests", function (accounts) {
-    it("Asseet address is set", function () {
+    it("Asset address is set", function () {
         return TokenTrader.deployed()
             .then((Instance) => {
-                Instance.asset.call()
+                return Instance.asset.call()
                     .then((address) => {
-                        assert.notEqual(address, "0x0");
-                        assert.notEqual(address, 0x0);
-                        assert.notEqual(address, undefined)
+                        assert.notEqual(address, "0x0000000000000000000000000000000000000000");
                     })
             })
-    })
+    });
+
+    it("Buy Price is set", function () {
+        return TokenTrader.deployed()
+            .then((Instance) => {
+                return Instance.buyPrice.call()
+                    .then((price) => {
+                        console.log(Number(price));
+                        assert.notEqual(Number(price), 0);
+                    })
+            })
+    });
+
+    it("Sell Price is set", function () {
+        return TokenTrader.deployed()
+            .then((Instance) => {
+                return Instance.sellPrice.call()
+                    .then((price) => {
+                        console.log(Number(price));
+                        assert.notEqual(Number(price), 0);
+                    })
+            })
+    });
+
+    it("Units are set", function () {
+        return TokenTrader.deployed()
+            .then((Instance) => {
+                return Instance.units.call()
+                    .then((unit) => {
+                        console.log(Number(unit));
+                        assert.notEqual(Number(unit), 0);
+                    })
+            })
+    });
+
+    it("Is selling tokens", function () {
+        return TokenTrader.deployed()
+            .then((Instance) => {
+                return Instance.sellsTokens.call()
+                    .then((_bool) => {
+                        console.log(_bool);
+                        assert.equal(_bool, true);
+                    })
+            })
+    });
+
+    it("Is buying tokens", function () {
+        return TokenTrader.deployed()
+            .then((Instance) => {
+                return Instance.buysTokens.call()
+                    .then((bool) => {
+                        console.log(bool);
+                        assert.equal(bool, true);
+                    })
+            })
+    });
 });
